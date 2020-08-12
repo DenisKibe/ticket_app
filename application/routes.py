7e83fbf4cfb71cca0703bbfb8992686d24ff22ce
Mr. Anonymous <kibedenis@gmail.com>
2020-08-12 12:25:41 +0300

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,25 @@
 from application import app
-from flask import render_template
+from flask import render_template, session, request, flash, url_for
+from application.models import User
+from application.forms import LoginForm
+import random, string
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get('Lsession'):
+        return redirect(url_for('/dashboard'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        
+        user = User.objects(email=email).first()
+        if user and user.get_password(password):
+            flash(f"{user.first_name}, Welcome")
+            session['Lsession'] = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
+            return redirect("/dashboard")
+        else:
+            flash("Sorry, something went wrong.", "danger")
+            
+    return render_template("Login.html")
