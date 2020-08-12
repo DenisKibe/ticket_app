b60a30dc2682acf52c9c222628fedeb026c5c8c4
Mr. Anonymous <kibedenis@gmail.com>
2020-08-12 16:09:51 +0300

    Update models.py

--- application/models.py
+++ application/models.py
@@ -1,19 +1,21 @@
 import flask
 from application import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from SQLAlchemy import Column, Integer, String, Float
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float, Text, TIMESTAMP, text
 
 class User(db.Model):
-  id= db.Column(db.Integer)
+  id= db.Column(db.Integer, primary_key=True)
   userId = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
   username = db.Column(db.String(80), nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   role = db.Column(db.String(30), nullable=False)
   
   
- class Ticket(db.Model):
+  
+class Ticket(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.String(20), db.foreignKey('User.userId'), nullable=False)
-  id = db.Column(db.Integer)
   ticketId=db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
   status = db.Column(db.String(15), nullable=False)
   image = db.Column(db.String(100), nullable=True)
@@ -21,4 +23,6 @@ class User(db.Model):
   category = db.Column(db.String(50))
   priority = db.Column(db.String(50), nullable=False)
   subject = db.Column(db.String(100), nullable=False)
+  created_at=db.Column(db.DateTime, server_default=text("CURRENT_TIMESTAMP"))
+  updated_at = db.Column(db.DateTime, server_default = text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),server_onupdate=FetchedValue())
   
