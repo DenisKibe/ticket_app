419e007ad789096f4243916e4222e976e9af9163
Mr. Anonymous <kibedenis@gmail.com>
2020-08-12 15:33:11 +0300

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,33 @@
-from application import app
-from flask import render_template, session, request, flash, url_for
-from application.models import User
+from application import app, db
+from flask import render_template, session, request, flash, url_for, json, Response
+from application.models import User, Ticket
 from application.forms import LoginForm
 import random, string
 
 @app.route("/")
 def index():
+    
+    return render_template("Llogin.html")
+
+@app.route("/dashboard/<user_id>"):
+    def dashboard(user_id=None):
+        if not session.get('Lsession'):
+            return redirect(url_for('login'))
+        
+        user = User.query.filter_by(userId=user_id).first();
+        
+        if(user.role == 'Admin'):
+            #some code
+        elif(user.role == 'Technician'):
+            #some code
+        elif(user.role == 'User'):
+            some code
+        
+        
+        
+@app.route("/login"):
     if session.get('Lsession'):
-        return redirect(url_for('/dashboard'))
+        return redirect(url_for('dashboard'))
     
     form = LoginForm()
     if form.validate_on_submit():
@@ -18,8 +38,13 @@ def index():
         if user and user.get_password(password):
             flash(f"{user.first_name}, Welcome")
             session['Lsession'] = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
-            return redirect("/dashboard")
+            return redirect("dashboard")
         else:
             flash("Sorry, something went wrong.", "danger")
             
-    return render_template("Login.html")
+            
+@app.route("/<user>/<urlto>"):
+    def urlto(user=None, urlto=None):
+        
+            
+    
