958aa787f0fd66ce9d847d86616edab19be5020e
Mr. Anonymous <kibedenis@gmail.com>
2020-08-12 13:04:45 +0300

    Update models.py

--- application/models.py
+++ application/models.py
@@ -1,3 +1,22 @@
 import flask
 from application import db
 from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+  id= db.Column(db.Integer)
+  userId = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
+  username = db.Column(db.String(80), nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  role = db.Column(db.String(30), nullable=False)
+  
+  
+ class Ticket(db.Model):
+  user_id = db.Column(db.String(20), db.foreignKey('User.userId'), nullable=False)
+  id = db.Column(db.Integer)
+  status = db.Column(db.String(15), nullable=False)
+  image = db.Column(db.String(100), nullable=True)
+  comment = db.Column(db.Text)
+  category = db.Column(db.String(50))
+  priority = db.Column(db.String(50), nullable=False)
+  subject = db.Column(db.String(100), nullable=False)
+  
