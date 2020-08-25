5d9bf125d022cdfebb0adf588db8d091aa71b307
DenisKibe <kibedenis@gmail.com>
2020-08-25 14:36:37 +0300

    add

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI='postgresql://kibe:denis@localhost/ticket_app'
+    SQLALCHEMY_DATABASE_URI='postgres://pxghwwnkuucdxy:8831774d0f4e2888c1056c8e2292436a083298a588979fd2eef7d4af2be380f0@ec2-184-72-162-198.compute-1.amazonaws.com:5432/deskio5b12lh1b'
     CSRF_ENABLED=True
     DEBUG = False
     TESTING = False
