2c07556f74490216cc6249533680d1553dd3c696
DenisKibe <kibedenis@gmail.com>
2020-09-08 16:12:16 +0300

    add

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI='postgres+pg5432://DenisKibe@nyuki:#Wanyugik18@nyuki.postgres.database.azure.com/ticketApp'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://DenisKibe@nyuki:#Wanyugik18@nyuki.postgres.database.azure.com:5432/ticketApp'
     CSRF_ENABLED=True
     DEBUG = False
     TESTING = False
