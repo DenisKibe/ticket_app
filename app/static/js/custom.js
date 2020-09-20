b6bfa5b3a16b2061e50c8d3104763bbca3fc7b89
DenisKibe <kibedenis@gmail.com>
2020-09-21 00:55:36 +0300

    add

---
+++ app/static/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+// SideNav Initialization
+$(".button-collapse").sideNav();
+
+var container = document.querySelector('.custom-scrollbar');
+var ps = new PerfectScrollbar(container, {
+wheelSpeed: 2,
+wheelPropagation: true,
+minScrollbarLength: 20
+});
+});
